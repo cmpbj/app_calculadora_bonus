1c995cc6c937826ab2e50783a6952caab680193b
cmpbj <magnocarlos3232@gmail.com>
2024-05-11 17:59:36 -0300

    Adicionar comentários no Script da aplicação

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import streamlit as st
 
 from calcula_bonus import bonus
 
+# Cria título para a aplicação
 st.title('Calculadora de Bônus')
 
+# Cria caixas para que o usuário possa inserir as informações
 nome = st.text_input('Digite seu nome: ')    
 salario = st.text_input(label='Digite seu salario: ')
 bonus_salario = st.text_input(label='Digite seu bônus: ')
 
 
-
+# Cria botão para controlar o fluxo do cálculo e poder fazer testes para o caso do usuário inserir informações errôneas
 if st.button("Calcular"):
     if nome.isalpha() == False:
         st.error('Digite um nome válido')

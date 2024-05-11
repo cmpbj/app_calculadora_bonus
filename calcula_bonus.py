f47d905e8c2a78207991a15d77dc0ad51011ecf0
cmpbj <magnocarlos3232@gmail.com>
2024-05-11 18:00:11 -0300

    Adicionar comentários ao Script com função que realiza o cálculo

--- calcula_bonus.py
+++ calcula_bonus.py
@@ -1,2 +1,4 @@
+# Cria função que recebe como parâmetro o salário do usuário e a porcentagem do bônus. Além disso, a função calcula o bônus em cima de um valor de 1000 reais
+# passado na função como uma constante
 def bonus(salario: float, porcentagem_bonus: float, CONSTANTE_BONUS=1000) -> float:
     return CONSTANTE_BONUS + (salario * porcentagem_bonus)

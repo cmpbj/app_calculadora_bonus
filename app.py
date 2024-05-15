dd184e13bfbeaa4521cf737b07a9b45ee71655ef
cmpbj <magnocarlos3232@gmail.com>
2024-05-14 22:31:21 -0300

    Adiciona algumas checagens para evitar erros do usuário

--- app.py
+++ app.py
@@ -10,16 +10,25 @@ nome = st.text_input('Digite seu nome: ')
 salario = st.text_input(label='Digite seu salario: ')
 bonus_salario = st.text_input(label='Digite seu bônus: ')
 
+# Remove caracteres especiais:
+
+bonus_salario = bonus_salario.replace(',', '.').replace('%', '')
+
 
 # Cria botão para controlar o fluxo do cálculo e poder fazer testes para o caso do usuário inserir informações errôneas
 if st.button("Calcular"):
-    if nome.isalpha() == False:
-        st.error('Digite um nome válido')
     try:
         salario = float(salario)
         bonus_salario = float(bonus_salario)
+        if bonus_salario > 1:
+            bonus_salario = bonus_salario / 100
+        else:
+            bonus_salario = bonus_salario
     except Exception as e:
         st.error('Digite números válidos')
-    else:
-        calcula_bonus_usuario = bonus(salario=salario, porcentagem_bonus=bonus_salario)
-        st.write(f"O usuário {nome} possui o bônus de {calcula_bonus_usuario: .2f}")
+    finally:
+        if nome.replace(" ", "").isalpha() == False or len(nome) == 0 or len(nome.strip()) == 0:
+            st.error('Digite um nome válido')
+        else:
+            calcula_bonus_usuario = bonus(salario=salario, porcentagem_bonus=bonus_salario)
+            st.write(f"O usuário {nome} possui um salário de {salario} e um bônus de {calcula_bonus_usuario: .2f}")
